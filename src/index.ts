c2c9485cfacdf9a060edbdf2b93d612a071944d8
beomki69 <beomki69@gmail.com>
2021-08-29 23:18:48 +0900

    add classes

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,22 @@
-interface Human {
-  name: string;
-  age: number;
-  gender: string;
+class Human {
+  public name: string;
+  public age: number;
+  public gender: string;
+  constructor(name: string, age: number, gender: string) {
+    this.name = name;
+    this.age = age;
+    this.gender = gender;
+  }
 }
 
-const person = {
-  name: "Ki",
-  age: 30,
-  gender: "male",
-};
+const ki = new Human("Ki", 30, "male");
+
 const sayHi = (person: Human): void => {
   console.log(
     `Hello ${person.name}, you are ${person.age} you are a ${person.gender}`
   );
 };
 
-sayHi(person);
+sayHi(ki);
 
 export {};
